c73d1d12e027d1fa9743c06bae7f7cbd5e89916e
thiagouft <thiagoufma@gmail.com>
2018-04-21 21:27:18 -0300

    Produtos e associação muitos para muitos

--- src/main/java/com/thiago/cursomc/CursomcApplication.java
+++ src/main/java/com/thiago/cursomc/CursomcApplication.java
@@ -1,6 +1,5 @@
 package com.thiago.cursomc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.thiago.cursomc.domain.Categoria;
+import com.thiago.cursomc.domain.Produto;
 import com.thiago.cursomc.repositories.CategoriaRepository;
+import com.thiago.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 	
 	@Autowired
-	CategoriaRepository categoriaRepositry;
+	private CategoriaRepository categoriaRepositry;
+	
+	@Autowired
+	private ProdutoRepository produtoRepositry;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -27,7 +31,19 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritório");
 		
+		Produto p1 = new Produto(null,"Computador", 2000.00);
+		Produto p2 = new Produto(null,"Impresora", 800.00);
+		Produto p3 = new Produto(null,"Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat1.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepositry.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepositry.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 }
--- src/main/java/com/thiago/cursomc/domain/Produto.java
+++ src/main/java/com/thiago/cursomc/domain/Produto.java
@@ -8,27 +8,37 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria implements Serializable{
+public class Produto implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private Double preco;
 	
-	@ManyToMany(mappedBy = "categorias")
-	private List<Produto> produtos = new ArrayList<>();
+	//Aqui é feito o mapeamento de uma tabela de n para n
 	
-	public Categoria() {
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA",
+		joinColumns = @JoinColumn(name = "produto_id"),
+		inverseJoinColumns = @JoinColumn(name = "categoria_id")
+	)
+	private List<Categoria> categorias = new ArrayList<>();
+	
+	public Produto() {
 	}
 
-	public Categoria(Integer id, String nome) {
+	public Produto(Integer id, String nome, Double preco) {
 		super();
 		this.id = id;
 		this.nome = nome;
+		this.preco = preco;
 	}
 
 	public Integer getId() {
@@ -47,6 +57,10 @@ public class Categoria implements Serializable{
 		this.nome = nome;
 	}
 
+	public Double getPreco() {
+		return preco;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -63,7 +77,7 @@ public class Categoria implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Produto other = (Produto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -72,13 +86,18 @@ public class Categoria implements Serializable{
 		return true;
 	}
 
-	public List<Produto> getProdutos() {
-		return produtos;
+	public void setPreco(Double preco) {
+		this.preco = preco;
 	}
 
-	public void setProdutos(List<Produto> produtos) {
-		this.produtos = produtos;
+	public List<Categoria> getCategorias() {
+		return categorias;
 	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}
+	
 	
 	
 
