114ffd3e1acb593facbc43ab4272106129c7bf3b
thiagouft <thiagoufma@gmail.com>
2018-04-24 01:01:15 -0300

    Estado e Cidade

--- src/main/java/com/thiago/cursomc/services/exception/ObjectNotFoundException.java
+++ src/main/java/com/thiago/cursomc/services/exception/ObjectNotFoundException.java
@@ -2,9 +2,6 @@ package com.thiago.cursomc.services.exception;
 
 public class ObjectNotFoundException extends RuntimeException {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
 	public ObjectNotFoundException(String msg) {
