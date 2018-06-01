23eeac0516a23782875a9478ea70e507c52639ec
thiagouft <thiagoufma@gmail.com>
2018-06-01 19:57:26 -0300

    Retirando importacoes indevidas

--- src/main/java/com/thiago/cursomc/resources/PedidoResources.java
+++ src/main/java/com/thiago/cursomc/resources/PedidoResources.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.thiago.cursomc.domain.Categoria;
 import com.thiago.cursomc.domain.Pedido;
-import com.thiago.cursomc.dto.CategoriaDTO;
 import com.thiago.cursomc.services.PedidoService;
 
 @RestController   //*Controlador Rest
