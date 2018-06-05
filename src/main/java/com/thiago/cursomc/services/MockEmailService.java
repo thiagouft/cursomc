46c09621c20a40a522fe98afead7835f9927d479
Thiago Silva Pereira <thiagoufma@gmail.com>
2018-06-05 01:53:59 -0300

    Email HTML

--- src/main/java/com/thiago/cursomc/services/MockEmailService.java
+++ src/main/java/com/thiago/cursomc/services/MockEmailService.java
@@ -1,7 +1,9 @@
 package com.thiago.cursomc.services;
 
-import org.slf4j.LoggerFactory;
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 
 public class MockEmailService extends AbstractEmailService {
@@ -11,6 +13,13 @@ public class MockEmailService extends AbstractEmailService {
 	@Override
 	public void sendEmail(SimpleMailMessage msg) {
 		LOG.info("Simulando envio de email...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado");
+	}
+	
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado");
 	}
